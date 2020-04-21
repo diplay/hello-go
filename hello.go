253509dcc320b8cb67c213c87c169f29ea4174a8
diplay <diplaySuper@gmail.com>
2020-04-21 21:58:13 +0300

    fix downloading different formats

--- hello.go
+++ hello.go
@@ -79,30 +79,41 @@ func findOutputFile(id string) string {
 }
 
 func doDownload(id, audioFormat string) (string, string, error) {
-	infoFilename := baseDir + id + ".info.json"
-	if _, err := os.Stat(infoFilename); os.IsNotExist(err) {
-		filename := "'" + baseDir + "%(id)s.%(ext)s'"
-		commandParams := "-x --write-info-json --no-progress -f 'bestaudio[filesize<20M]/best[filesize<20M]/worstaudio/worst' -o " + filename
-		if len(audioFormat) > 0 {
-			commandParams += " --audio-format '" + audioFormat + "'"
+	var fileToDeleteIfDownloadSecceed string
+	if resultFilename := findOutputFile(id); len(resultFilename) > 0 {
+		log.Printf("Found existing file %s\n", resultFilename)
+		if len(audioFormat) == 0 || (len(audioFormat) > 0 && strings.HasSuffix(resultFilename, audioFormat)) {
+			return resultFilename, "", nil
 		}
-		command := commandName + " " + commandParams + " -- " + id
-		cmd := exec.Command("sh", "-c", command)
 
-		log.Printf("Run %s\n", command)
-		out, err := cmd.Output()
-
-		log.Printf("The %s output is\n%s\n", command, out)
-
-		return findOutputFile(id), command, err
+		log.Printf("Existing file %s does not match wanted type %s\n", resultFilename, audioFormat)
+		fileToDeleteIfDownloadSecceed = baseDir + resultFilename
 	}
 
+	filename := "'" + baseDir + "%(id)s.%(ext)s'"
+	commandParams := "-x --write-info-json --no-progress -f 'bestaudio[filesize<20M]/best[filesize<20M]/worstaudio/worst' -o " + filename
+	if len(audioFormat) > 0 {
+		commandParams += " --audio-format '" + audioFormat + "'"
+	}
+	command := commandName + " " + commandParams + " -- " + id
+	cmd := exec.Command("sh", "-c", command)
+
+	log.Printf("Run %s\n", command)
+	out, err := cmd.Output()
+	log.Printf("The %s output is\n%s\n", command, out)
+
 	if resultFilename := findOutputFile(id); len(resultFilename) > 0 {
-		return resultFilename, "", nil
+		if len(fileToDeleteIfDownloadSecceed) > 0 {
+			err := os.RemoveAll(fileToDeleteIfDownloadSecceed)
+			if err != nil {
+				log.Printf("Cannot delete %s", resultFilename)
+			}
+		}
+		return resultFilename, "", err
 	}
 
 	log.Printf("Cannot find output file for video %s", id)
-	return "", "", nil
+	return "", "", err
 }
 
 func downloadHandle(w http.ResponseWriter, r *http.Request) {
